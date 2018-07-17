44d39bc53c0c2f3d6debd93e87d1e8dc83ff9d7e
System Administrator <root@MacBook-Air-3.local>
2018-07-16 23:22:47 -0400

    adding forms

--- water_system.py
+++ water_system.py
@@ -8,6 +8,9 @@ import pandas as pd
 import json
 import pysd
 import urllib2
+import gviz_api
+from gviz_data_table import Table
+
 
 templateDir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates');
 app = Flask (__name__, template_folder=templateDir);
@@ -24,18 +27,37 @@ def result():
     params= {
 	'final_time':11.23,
 	'heat_loss_to_room':67.98,
-	'saveper':87.9,
-	'time_step':2,
-	'initial_time': 23.238
-	}  
+	'saveper':23.2,
+	'time_step':104.2,
+	'initial_time': 12.34, 
+ 	'final_time':7.8	
+      }  
     return_columns = [
 	'characteristic_time', 
 	'room_temperature',
 	'teacup_temperature'
 	]   
- 
+   
     data = model.run(params=params,return_columns=return_columns)
+    table = Table()
+    table.add_column('characteristic_time', int, 'CT')
+    table.add_column('room_temperature', int, 'RT')
+    table.add_column('teacup_temperature', int, 'TT')
+    for row in data.iterrows():
+        print row[1].tolist()
+    	table.append(row[1].tolist())
+   
+    for key,value in table:
+	print (key,value)
     return render_template('well.html', data=data)
+    
+'''
+    description= {("characteristic_time"):[('value')]}
+    data_table= gviz_api.DataTable(description)
+    data_table.LoadData(table) 
+    json= data_table.ToJSon()
+'''
+ #   return render_template('well.html', data=data)
 
 if __name__ == '__main__':
     app.run(debug=True)
